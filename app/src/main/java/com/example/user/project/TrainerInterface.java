79509207633450f5bdaf877844fea1d8b041cddd
Rishni <Rishni@DESKTOP-7681NTT>
2018-06-28 23:57:30 +0530

    fitness questionnaire ui

--- app/src/main/java/com/example/user/project/TrainerInterface.java
+++ app/src/main/java/com/example/user/project/TrainerInterface.java
@@ -1,7 +1,9 @@
 package com.example.user.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TrainerInterface extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class TrainerInterface extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainer_interface);
+    }
+
+    public void fitness_clicked(View view)
+    {
+        if(view.getId()==R.id.fitness_btn)
+        {
+            Intent intent=new Intent(this,FitnessQuestionnaire.class);
+            startActivity(intent);
+        }
     }
 }
