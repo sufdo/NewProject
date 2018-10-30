4766d4646a9a1bb6e36129479030fbb46cafc30d
Rishni <Rishni@DESKTOP-7681NTT>
2018-10-30 15:54:03 +0530

    fitness ui and tsb modification

--- app/src/main/java/com/example/user/project/FitnessQuestionnaire.java
+++ app/src/main/java/com/example/user/project/FitnessQuestionnaire.java
@@ -1,7 +1,9 @@
 package com.example.user.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FitnessQuestionnaire extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class FitnessQuestionnaire extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle(R.string.fitness_q);
         setContentView(R.layout.activity_fitness_questionnaire);
+    }
+
+    public void fitnessSubmit_clicked(View view)
+    {
+        if(view.getId()==R.id.fitnessSubmit)
+        {
+            Intent intent=new Intent(this,TSB.class);
+            startActivity(intent);
+        }
     }
 }
