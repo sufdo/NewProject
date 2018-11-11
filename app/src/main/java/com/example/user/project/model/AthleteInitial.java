84c4603e48266869a6b23a5eba8aab821c6c2b66
Rishni <Rishni@DESKTOP-7681NTT>
2018-11-12 02:58:19 +0530

    TSB

---
+++ app/src/main/java/com/example/user/project/model/AthleteInitial.java
@@ -0,0 +1,67 @@
+package com.example.user.project.model;
+
+public class AthleteInitial {
+    private String id;
+    private String name;
+    private String nic;
+    private String joinedDate;
+    private String dob;
+    private String trainerID;
+    private String approved;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(String joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTrainerID() {
+        return trainerID;
+    }
+
+    public void setTrainerID(String trainerID) {
+        this.trainerID = trainerID;
+    }
+
+    public String getApproved() {
+        return approved;
+    }
+
+    public void setApproved(String approved) {
+        this.approved = approved;
+    }
+}
