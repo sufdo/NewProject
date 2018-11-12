d5d098b62639a11f2a5ddec8d4703ed2ecad6ab3
Rishni <Rishni@DESKTOP-7681NTT>
2018-11-12 06:20:34 +0530

    TSB new

--- app/src/main/java/com/example/user/project/FitnessQuestionnaire.java
+++ app/src/main/java/com/example/user/project/FitnessQuestionnaire.java
@@ -18,7 +18,6 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.example.user.project.model.AthleteInitial;
 import com.example.user.project.model.Fitness;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -49,8 +48,7 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
     String ServerURL;
     String id,athlete,nic,date,dob,trainer,approved;
     String trainerID;
-
-    List<AthleteInitial> athleteList=new ArrayList<AthleteInitial>();
+    //String athleteArr[];
     List athleteArr =new ArrayList();
     Spinner spinner;
     ArrayAdapter<String> adapter;
@@ -92,6 +90,17 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
 
         mProgressBar=(ProgressBar)findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.GONE);
+
+        /**
+         Spinner spinner= findViewById(R.id.spinner);
+         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.athletes,android.R.layout.simple_spinner_item);
+         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         spinner.setAdapter(adapter);
+         spinner.setOnItemSelectedListener(this);
+         **/
+
+        //String athleteArr[]={"athlete1","athlete2","athlete3"};
+
 
         spinner= findViewById(R.id.spinner);
         adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,athleteArr);
@@ -171,15 +180,15 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
             Date date=new Date();
             currentDate=dateFormat.format(date);
             /**
-            toastMessage("trainerID ="+trainerID);
-            toastMessage("athleteNIC ="+nic);
-            toastMessage("athleteName ="+athleteName);
-            toastMessage("shuttleLoad ="+shuttleLoad);
-            toastMessage("type ="+type);
-            toastMessage("flightTime ="+flightTime);
-            toastMessage("fatigueLevel ="+fatigueLevel);
-            toastMessage("currentDate ="+currentDate);
-            **/
+             toastMessage("trainerID ="+trainerID);
+             toastMessage("athleteNIC ="+nic);
+             toastMessage("athleteName ="+athleteName);
+             toastMessage("shuttleLoad ="+shuttleLoad);
+             toastMessage("type ="+type);
+             toastMessage("flightTime ="+flightTime);
+             toastMessage("fatigueLevel ="+fatigueLevel);
+             toastMessage("currentDate ="+currentDate);
+             **/
 
             Boolean result=validation.isEmpty(athleteName,shuttleLoad,type,flightTime,fatigueLevel);
             if(result.equals(false))
@@ -268,7 +277,8 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
                 dialog.show();
             }
 
-
+            //Intent intent=new Intent(this,TSB.class);
+            //startActivity(intent);
         }
     }
 
@@ -292,6 +302,7 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
         }
         @Override
         protected Void doInBackground(Void... voids) {
+            ServerURL = "https://murmuring-cove-69371.herokuapp.com/athleteInit/trainerID/"+trainerID;
             doGet();
             return null;
         }
@@ -299,77 +310,57 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
         {
             try
             {
-                URL url = new URL("https://murmuring-cove-69371.herokuapp.com/athleteInit/trainerID/"+trainerID);
+                URL url = new URL(ServerURL);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept","application/json");
 
-                //check the connection status
-                if(conn.getResponseCode()==200)
-                {
-                    //if response code=200 - HTTP.OK
-                    InputStream in=new BufferedInputStream(conn.getInputStream());
+                InputStream responseBody = conn.getInputStream();
+                InputStreamReader responseBodyReader = new InputStreamReader(responseBody,"UTF-8");
+                JsonReader jsonReader = new JsonReader(responseBodyReader);
+                //jsonReader.beginArray();
+                jsonReader.beginArray();
+                while (jsonReader.hasNext()){
+                    jsonReader.beginObject();
 
-                    //read the BufferedInputStream
-                    BufferedReader r=new BufferedReader(new InputStreamReader(in));
-                    StringBuilder sb=new StringBuilder();
-                    String line;
-                    while ((line=r.readLine())!=null)
-                    {
-                        sb.append(line);
+                    while( jsonReader.hasNext() ) {
+                        String idName = jsonReader.nextName();
+                        id = jsonReader.nextString();
+                        String athleteName = jsonReader.nextName();
+                        athlete = jsonReader.nextString();
+                        athleteArr.add(athlete);
+                        String nicName = jsonReader.nextName();
+                        nic = jsonReader.nextString();
+                        String dateName = jsonReader.nextName();
+                        date = jsonReader.nextString();
+                        String dobName = jsonReader.nextName();
+                        dob = jsonReader.nextString();
+                        String trainerName = jsonReader.nextName();
+                        trainer = jsonReader.nextString();
+                        String approvedName = jsonReader.nextName();
+                        approved = jsonReader.nextString();
+
                     }
-                    stream=sb.toString();
-                    conn.disconnect();
-                }
 
-                else
-                {
 
                 }
 
-                Gson gson=new Gson();
-                Type listType=new TypeToken<List<AthleteInitial>>(){}.getType();
-                athleteList=gson.fromJson(stream,listType);//parse to list
-
-
+                jsonReader.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+
         }
         protected void onPostExecute(Void voids) {
             super.onPostExecute(voids);
-
-
-            if(!athleteList.isEmpty())
-            {
-                Log.i("FITNESS_Q", "size"+athleteList.size());
-
-                for(int i=0;i<(athleteList.size()-1);i++)
-                {
-                    Log.i("FITNESS_Q", "name"+athleteList.get(i).getName());
-                    Log.i("FITNESS_Q" , "nic"+athleteList.get(i).getNic());
-                    if(athleteArr.isEmpty())
-                    {
-                        athleteArr.add(athleteList.get(i).getName());
-                    }
-                    else
-                    {
-                        if(!athleteArr.contains(athleteList.get(i).getName()))
-                        {
-                            athleteArr.add(athleteList.get(i).getName());
-                        }
-                    }
-                }
-                spinner.setAdapter(adapter);
-                mProgressBar.setVisibility(View.GONE);
-            }
-            else
-            {
-
-            }
-
-
+            spinner.setAdapter(adapter);
+            mProgressBar.setVisibility(View.GONE);
         }
     }
+
     //post fitness details
     class SendData extends AsyncTask {
 
@@ -399,10 +390,10 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
                 jsonParam.put("flightTime", flightTime);
                 jsonParam.put("fatigueLevel", fatigueLevel);
                 jsonParam.put("fitnessPerDay", fitnessPerDay);
-                jsonParam.put("CTL", CTL);
+                jsonParam.put("ctl", CTL);
                 jsonParam.put("fatiguePerDay", fatiguePerDay);
-                jsonParam.put("ATL", ATL);
-                jsonParam.put("TSB", TSB);
+                jsonParam.put("atl", ATL);
+                jsonParam.put("tsb", TSB);
                 jsonParam.put("date", currentDate);
 
 
@@ -423,14 +414,7 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
 
                 Log.i("STATUS", String.valueOf(conn.getResponseCode()));
                 Log.i("MSG" , conn.getResponseMessage());
-                if(conn.getResponseCode()==200)
-                {
-                    message="Successfully saved";
-                }
-                else
-                {
-                    message="Error";
-                }
+
                 conn.disconnect();
 
             } catch (MalformedURLException e) {
@@ -446,11 +430,11 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
 
         protected void onPostExecute(Object object) {
             super.onPostExecute(object);
-            toastMessage(message);
-            Intent intent=new Intent(FitnessQuestionnaire.this,TSB.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            finish();
+            toastMessage("Successfully saved");
+            //Intent intent = new Intent(InjuryAdd.this, Injury_View.class);
+            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            //startActivity(intent);
+            //finish();
         }
     }
 
@@ -493,7 +477,7 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
 
                 else
                 {
-                   // message="error not saved";
+                    // message="error not saved";
                 }
 
                 Gson gson=new Gson();
@@ -520,17 +504,15 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
                         Log.i("FATIGUE", "date"+fitnessList.get(i).getDate());
                         Log.i("FATIGUE" , "fitnessPerDay"+fitnessList.get(i).getFitnessPerDay());
                         Log.i("FATIGUE", "fatiguePerDay"+fitnessList.get(i).getFatiguePerDay());
-                        Log.i("FATIGUE" , "CTL"+fitnessList.get(i).getCTL());
-                        Log.i("FATIGUE" , "ATL"+fitnessList.get(i).getATL());
+                        Log.i("FATIGUE" , "CTL"+fitnessList.get(i).getCtl());
+                        Log.i("FATIGUE" , "ATL"+fitnessList.get(i).getAtl());
 
                         ATLdouble=ATLdouble+Double.parseDouble(fitnessList.get(i).getFatiguePerDay());
                         Log.i("FATIGUE" , "ATLdouble"+ATLdouble);
                         /**count +=1;
                          Log.i("FATIGUE", "count"+count);
-
                          if(count>=6)
                          {
-
                          }**/
                     }
 
@@ -545,8 +527,8 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
                             Log.i("FITNESS", "date"+fitnessList.get(i).getDate());
                             Log.i("FITNESS" , "fitnessPerDay"+fitnessList.get(i).getFitnessPerDay());
                             Log.i("FITNESS", "fatiguePerDay"+fitnessList.get(i).getFatiguePerDay());
-                            Log.i("FITNESS" , "CTL"+fitnessList.get(i).getCTL());
-                            Log.i("FITNESS" , "ATL"+fitnessList.get(i).getATL());
+                            Log.i("FITNESS" , "CTL"+fitnessList.get(i).getCtl());
+                            Log.i("FITNESS" , "ATL"+fitnessList.get(i).getAtl());
 
                             CTLdouble=CTLdouble+Double.parseDouble(fitnessList.get(i).getFitnessPerDay());
                             Log.i("FITNESS" , "CTLdouble"+CTLdouble);
@@ -558,6 +540,11 @@ public class FitnessQuestionnaire extends AppCompatActivity implements AdapterVi
 
                         TSBLong=Math.round(CTLdouble-ATLdouble);
                         TSB=Long.toString(TSBLong);
+                    }
+                    else
+                    {
+                        CTL="null";
+                        TSB="null";
                     }
                 }
                 else
